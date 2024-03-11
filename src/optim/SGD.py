b1107d9c2e1ac40df06e6f3c0bfef3f6a0a0ae88
dyrewucktv <mikolaj@roguski.eu>
2024-03-11 18:30:19 +0100

    uniform naming of stop_condition in iwls and sgd

--- src/optim/SGD.py
+++ src/optim/SGD.py
@@ -2,14 +2,14 @@ import numpy as np
 
 
 class SGD:
-    def __init__(self, model, stopping_condition, learning_rate=.001, batch_size=1):
+    def __init__(self, model, stop_condition, learning_rate=.001, batch_size=1):
         self.model = model
-        self.stopping_condition = stopping_condition
+        self.stop_condition = stop_condition
         self.learning_rate = learning_rate
         self.batch_size = batch_size
 
     def optimize(self, x, y):
-        while not self.stopping_condition(model=self.model, x=x, y=y):
+        while not self.stop_condition(model=self.model, x=x, y=y):
             sample = np.random.choice(range(len(y)), self.batch_size)
             probs = self.model.predict_probs(x[sample, :])
             self.model.weights = self.model.weights - self.learning_rate * np.mean(
