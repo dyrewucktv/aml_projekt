0e76d7e92f5d05830fdbd8df9ebbc611ed0df849
dyrewucktv <mikolaj@roguski.eu>
2024-03-11 18:26:48 +0100

    added stopping condition for no loglik improvement

--- src/optim/conditions.py
+++ src/optim/conditions.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class MaxIterationCondition:
@@ -9,4 +10,24 @@ class MaxIterationCondition:
         self.iteration += 1
         if self.iteration > self.max_iterations:
             return True
+        return False
+
+
+class NoLogLikImprovementCondition:
+    def __init__(self, no_loglik_improvement_iters=1):
+        self.no_loglik_improvement_iters = no_loglik_improvement_iters
+        self.last_scores = []
+        self.epoch = -2
+
+    def __call__(self, model=None, x=None, y=None, **kwargs):
+        self.epoch += 1
+        if self.epoch < 0:
+            return False
+        loglik = np.sum(-np.log(np.exp(model.weights @ x.T)) + y * (model.weights @ x.T))
+        if self.last_scores:
+            if loglik <= np.mean(self.last_scores) and len(self.last_scores) >= self.no_loglik_improvement_iters:
+                return True
+            if len(self.last_scores) == self.no_loglik_improvement_iters:
+                self.last_scores.pop(0)
+        self.last_scores.append(loglik)
         return False
