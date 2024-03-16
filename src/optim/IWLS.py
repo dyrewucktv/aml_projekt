160d751d25c8b43dcf839ed20ae55899794daf9e
dyrewucktv <mikolaj@roguski.eu>
2024-03-17 00:13:02 +0100

    new condition more robust iwls cleaned up test notebook

--- src/optim/IWLS.py
+++ src/optim/IWLS.py
@@ -2,16 +2,19 @@ import numpy as np
 
 
 class IWLS:
-    def __init__(self, model, stop_condition, delta=1e-5):
+    def __init__(self, model, stop_condition, delta=1e-5, lambda_=1e-5):
         self.model = model
         self.stop_condition = stop_condition
         self.delta = delta
+        self.lambda_ = 1e-5
 
     def optimize(self, x, y):
         while not self.stop_condition(model=self.model, x=x, y=y):
             p = self.model.predict_probs(x)
             w = np.diag(np.maximum(p * (1 - p), self.delta))
             z = x @ self.model.weights + np.linalg.inv(w) @ (y - p)
-            b_new = np.linalg.inv(x.T @ w @ x) @ x.T @ w @ z
+            b_new = np.linalg.inv(
+                (1 - self.lambda_) * (x.T @ w @ x)
+                + self.lambda_ * np.identity(self.model.weights.shape[0])) @ x.T @ w @ z
             self.model.weights = b_new
         return self.model
--- src/optim/conditions.py
+++ src/optim/conditions.py
@@ -14,10 +14,10 @@ class MaxIterationCondition:
 
 
 class NoLogLikImprovementCondition:
-    def __init__(self, no_loglik_improvement_iters=1):
-        self.no_loglik_improvement_iters = no_loglik_improvement_iters
+    def __init__(self, threshold=1e-6):
         self.last_scores = []
         self.epoch = -2
+        self.threshold = threshold
 
     def __call__(self, model=None, x=None, y=None, **kwargs):
         self.epoch += 1
@@ -25,9 +25,18 @@ class NoLogLikImprovementCondition:
             return False
         loglik = np.sum(-np.log(np.exp(model.weights @ x.T)) + y * (model.weights @ x.T))
         if self.last_scores:
-            if loglik <= np.mean(self.last_scores) and len(self.last_scores) >= self.no_loglik_improvement_iters:
+            if loglik - self.last_scores[-1] <= self.threshold:
                 return True
-            if len(self.last_scores) == self.no_loglik_improvement_iters:
-                self.last_scores.pop(0)
         self.last_scores.append(loglik)
         return False
+
+
+class NoLogLikOrMaxIterCondition(NoLogLikImprovementCondition):
+    def __init__(self, max_iterations=10, threshold=1e-6):
+        super().__init__(threshold)
+        self.max_iterations = max_iterations
+
+    def __call__(self, model=None, x=None, y=None, **kwargs):
+        if self.epoch > self.max_iterations:
+            return True
+        super().__call__(model=model, x=x, y=y, **kwargs)
