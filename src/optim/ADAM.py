fixed loglikelihood, introduced a common superclass added accuracy to scoring

src/optim/ADAM.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+from src.optim.BaseOptimizer import BaseOptimizer
 
-class ADAM:
-    def __init__(self, model, stop_condition, learning_rate=.001, beta1=.9, beta2=.999, epsilon=1e-8, batch_size=1):
+
+class ADAM(BaseOptimizer):
+    def __init__(self, model, stop_condition, learning_rate=.01, beta1=.9, beta2=.999, epsilon=1e-8, batch_size=1):
+        super().__init__(model, stop_condition)
         self.model = model
         self.stop_condition = stop_condition
         self.learning_rate = learning_rate
@@ -28,7 +31,6 @@ class ADAM:
                 m_hat = self.m / (1 - self.beta1 ** self.t)
                 v_hat = self.v / (1 - self.beta2 ** self.t)
                 self.model.weights = self.model.weights - self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
-            prediction = np.clip(self.model.predict(x), 1e-10, 1 - 1e-10)
-            loglik = - np.mean(y * np.log(prediction) + (1 - y) * np.log(1 - prediction))
+            loglik, accuracy = self.score(x, y)
             logliks.append(loglik)
         return self.stop_condition.best_model if hasattr(self.stop_condition, 'best_model') else self.model, logliks

src/optim/IWLS.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+from src.optim.BaseOptimizer import BaseOptimizer
 
-class IWLS:
-    def __init__(self, model, stop_condition, delta=1e-5, lambda_=1e-5):
-        self.model = model
-        self.stop_condition = stop_condition
-        self.delta = delta
+
+class IWLS(BaseOptimizer):
+
+    def __init__(self, model, stop_condition, lambda_=1e-10, delta=1e-10):
+        super().__init__(model, stop_condition)
         self.lambda_ = lambda_
+        self.delta = delta
 
     def optimize(self, x, y):
         logliks = []
@@ -18,7 +20,6 @@ class IWLS:
                 (1 - self.lambda_) * (x.T @ (x * w[:, np.newaxis]))
                 + self.lambda_ * np.identity(self.model.weights.shape[0])) @ x.T @ (z * w[:, np.newaxis])
             self.model.weights = np.squeeze(b_new)
-            prediction = np.clip(self.model.predict(x), 1e-10, 1 - 1e-10)
-            loglik = - np.mean(y * np.log(prediction) + (1 - y) * np.log(1 - prediction))
+            loglik, accuracy = self.score(x, y)
             logliks.append(loglik)
         return self.stop_condition.best_model if hasattr(self.stop_condition, 'best_model') else self.model, logliks

src/optim/SGD.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+from src.optim.BaseOptimizer import BaseOptimizer
 
-class SGD:
-    def __init__(self, model, stop_condition, learning_rate=.001, batch_size=1):
+
+class SGD(BaseOptimizer):
+    def __init__(self, model, stop_condition, learning_rate=.01, batch_size=1):
+        super().__init__(model, stop_condition)
         self.model = model
         self.stop_condition = stop_condition
         self.learning_rate = learning_rate
@@ -17,7 +20,6 @@ class SGD:
                 probs = self.model.predict_probs(x[sample, :])
                 self.model.weights = self.model.weights - self.learning_rate * np.mean(
                     np.mean(probs - y[sample]) * x[sample], axis=0)
-            prediction = np.clip(self.model.predict(x), 1e-10, 1 - 1e-10)
-            loglik = - np.mean(y * np.log(prediction) + (1 - y) * np.log(1 - prediction))
+            loglik, accuracy = self.score(x, y)
             logliks.append(loglik)
         return self.stop_condition.best_model if hasattr(self.stop_condition, 'best_model') else self.model, logliks

src/optim/conditions.py
@@ -26,7 +26,7 @@ class NoLogLikImprovementCondition:
         self.epoch += 1
         if self.epoch < 0:
             return False
-        prediction = np.clip(model.predict(x), 1e-10, 1 - 1e-10)
+        prediction = np.clip(model.predict_probs(x), 1e-10, 1 - 1e-10)
         loglik = - np.mean(y * np.log(prediction) + (1 - y) * np.log(1 - prediction))
         if loglik >= self.best_score and self.epoch - self.last_improvement_epoch >= self.patience:
             return True